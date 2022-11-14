d6f45d1bb48ceaa34bc488e9e08058db76da92b7
sherzod02 <diyorbekolimov2000@gmail.com>
2022-11-14 07:25:56 +0500

    Search panel component Done

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -35,6 +35,7 @@ class App extends Component {
           like: false,
         },
       ],
+      term: "",
     };
   }
 
@@ -64,10 +65,21 @@ class App extends Component {
     }));
   };
 
+  searchHandler = (arr, term) => {
+    if (term.length === 0) {
+      return arr;
+    }
+
+    return arr.filter((item) => item.name.toLowerCase().indexOf(term) > -1);
+  };
+
+  updateTermHandler = (term) => this.setState({ term });
+
   render() {
-    const { data } = this.state;
+    const { data, term } = this.state;
     const allMoviesCount = data.length;
     const favouriteMovieCount = data.filter((c) => c.favourite).length;
+    const visibleData = this.searchHandler(data, term);
 
     return (
       <div className="app font-monospace">
@@ -77,12 +89,12 @@ class App extends Component {
             favouriteMovieCount={favouriteMovieCount}
           />
           <div className="search-panel">
-            <SearchPanel />
+            <SearchPanel updateTermHandler={this.updateTermHandler} />
             <AppFilter />
           </div>
           <MovieList
             onToggleProp={this.onToggleProp}
-            data={data}
+            data={visibleData}
             onDelete={this.onDelete}
           />
           <MoviesAddForm addForm={this.addForm} />
--- src/components/search-panel/Search-panel.jsx
+++ src/components/search-panel/Search-panel.jsx
@@ -1,9 +1,29 @@
-import "./Search-panel.css"
+import { Component } from "react";
+import "./Search-panel.css";
 
-const SearchPanel = () => {
-  return (
-    <input type="text" className="form-control search-input"  placeholder="Kinolarni qidirish"/>
-  )
+class SearchPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: "" };
+  }
+
+  updateTermHandler = (e) => {
+    const term = e.target.value.toLowerCase();
+    this.setState({ term });
+    this.props.updateTermHandler(term);
+  };
+
+  render() {
+    return (
+      <input
+        type="text"
+        className="form-control search-input"
+        placeholder="Kinolarni qidirish"
+        onChange={this.updateTermHandler}
+        value={this.state.term}
+      />
+    );
+  }
 }
 
-export default SearchPanel
+export default SearchPanel;
